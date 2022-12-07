abb31cbdce92463fa6c1d6cf575bdbfcfec88cea
shamsasaeed <98390974+shamsasaeed@users.noreply.github.com>
2022-12-07 18:35:49 +0400

    Add files via upload

---
+++ MyWebsiteSep22/script.js
@@ -0,0 +1,10 @@
+
+/* first page mouse over effect to change text */
+
+function mOver(obj) {
+  obj.innerHTML = "Thank you for being here <3"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Scroll here"
+}
